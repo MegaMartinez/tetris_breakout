6f40eaad05158a8d9544b8427c1750ae4ddc293f
rhit-martinms <martinms@rose-hulman.edu>
2021-11-05 17:31:57 -0400

    quick fix

--- 02_Model_View_Controller_starting_code/filesystem.py
+++ 02_Model_View_Controller_starting_code/filesystem.py
@@ -11,9 +11,6 @@ if path == "99z-Capstone_Team_Project-202210-05-martinms-fletchj1-aquaslc":
 else:
     ide = "pycharm"
 
-def thing():
-    pass
-
 def file(file):
     file = str(file)
     if ide == "vscode":
