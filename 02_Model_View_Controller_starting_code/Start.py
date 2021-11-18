29b22ce037d757ab884380e8b67b178cad3f2c19
rhit-martinms <martinms@rose-hulman.edu>
2021-11-18 16:27:11 -0500

    menu done

--- 02_Model_View_Controller_starting_code/Start.py
+++ 02_Model_View_Controller_starting_code/Start.py
@@ -1,6 +1,11 @@
 import pygame
 from Scoreboard import Scoreboard
 import math
+from filesystem import file
+
+title = pygame.image.load(file("title.png"))
+tutorial = pygame.image.load(file("tutorial.png"))
+message = pygame.image.load(file("intromessage.png"))
 
 class Start:
     def __init__(self, screen):
@@ -16,8 +21,12 @@ class Start:
         score_image2 = self.font.render(score_string2, True, (255, 255, 255))
         score_image3 = self.font.render(score_string3, True, (255, 255, 255))
         score = self.font.render(score_letter, True, (255, 255, 255))
-        self.screen.blit(score_image1, (100, 30))
-        self.screen.blit(score_image2, (100, 50))
-        self.screen.blit(score_image3, (100, 70))
-        self.screen.blit(score, (100, 15))
+        self.screen.blit(score_image1, (100, 55))
+        self.screen.blit(score_image2, (100, 75))
+        self.screen.blit(score_image3, (100, 95))
+        self.screen.blit(score, (100, 40))
+        self.screen.blit(title, (0, 0))
+        self.screen.blit(tutorial, (8, 40))
+        self.screen.blit(message, (0, 160))
+
 
